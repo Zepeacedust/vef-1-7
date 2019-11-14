33cbcc9bcf0e7c97a1bdb3ebaa0741152d83c8d7
Arnor <arnor0903@gmail.com>
2019-11-14 14:26:19 +0000

    gleymdi mér smá, kominn með signup og paart af login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template as rend, session, request
 import hashlib, binascii, os, pymysql,secrets
 app = Flask(__name__)
-
+#todo connecta rétt
 connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='pass.123', database='users', autocommit=True)
 cursor = connection.cursor()
 #alltaf gaman að fara overboard
@@ -27,13 +27,11 @@ def verify_password(stored_password, provided_password):
     return pwdhash == stored_password
 
 def verify():
-    with connection: 
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM users")
-        rows = cur.fetchall()
-        for row in rows:
-            print("{0} {1} {2}".format(row[0], row[1], row[2]))
-
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        print("{0} {1} {2}".format(row[0], row[1], row[2]))
+     
 @app.route('/')
 def index():
     #todo setja up user verification
@@ -42,11 +40,41 @@ def index():
     else:
         user = session["user"]
     return rend("index.html", user=user)
+@app.route("/signup")
+def signup():
+    return rend("signup.html",code=None)
+@app.route("/signup/create",methods=['POST'])
+def addusr():
+    r=request.form
+    print(r)
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    if request.form['username'] in map(lambda x: x[0], rows):
+        pass
+    cursor.execute("""INSERT INTO users (user, pass, nafn) VALUES
+    (%s, %s, %s);""" % 
+    (connection.escape(request.form['username']),
+    connection.escape(hash_password(request.form['password'])),
+    connection.escape(request.form['name'])))
+    return rend("signup.html",code=0)
 
 @app.route("/login")
+def login_page():
+    return rend("login.html",code = None)
+@app.route("/login/process",methods=['POST'])
 def login():
-    #todo setja upp logins
-    return index()
+    r=request.form
+    cursor.execute("""select * from users where user=%s;""" % (
+        connection.escape(request.form['username'])))
+    rows = cursor.fetchall()
+    if rows:
+        user = rows[0]
+        session["user"] = {"name":user[0],"pass":user[1],"nafn":user[2]}
+        print(user)
+    else: 
+        print("no")
+        return rend("login.html", code=1)
+    return rend("login.html", code=0)
 
 if __name__ == '__main__':
     app.run()
