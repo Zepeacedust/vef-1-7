c3d855fab88724856247793261de8da4ad642f62
Arnor <arnor0903@gmail.com>
2019-11-14 11:13:06 +0000

    Ragner er fool

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import hashlib, binascii, os, pymysql,secrets
 app = Flask(__name__)
 
 connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='pass.123', database='users', autocommit=True)
+cursor = connection.cursor()
 #alltaf gaman að fara overboard
-app.secret_key = secrets.token_hex(2048)
+app.secret_key = secrets.token_hex(255)
 def authorize(user):
     pass
     #todo tengja við localhost, muna að breyta aftur
+
 ##stal smá kóða af fólki sem veit betur
 def hash_password(password):
     #Hash a password for storing.
@@ -24,11 +26,19 @@ def verify_password(stored_password, provided_password):
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
 
+def verify():
+    with connection: 
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM users")
+        rows = cur.fetchall()
+        for row in rows:
+            print("{0} {1} {2}".format(row[0], row[1], row[2]))
+
 @app.route('/')
 def index():
     #todo setja up user verification
     if ~("user" in session):
-        user = {"name":"kalli","pass":"pass","email":"kallik@fkmail.is"}
+        user = {"name":None,"pass":None,"email":None}
     else:
         user = session["user"]
     return rend("index.html", user=user)
